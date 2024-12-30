e4083df4e82339221e0c902f0a29f4c6a3256ef5
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-12-30 22:10:05 +0500

    JAVASCRIPT STARTED

---
+++ day13.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+
+
+console.log(10 + 20, "YEH ADDITION HAI")
+
+console.log(10 - 5)
+console.log(10 * 5)
+console.log(10 / 5)
+
+
+console.log("Hello, My name is Muhammad Zohaib and I am 24 Years old.")
+
+
+var userName = "Ubaidullah" 
+var userAge = 18 + 1
+
+
+console.log("Hello, My name is " + userName + " and I am " + userAge + " years old.")
