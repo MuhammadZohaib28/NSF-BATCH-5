7923a1633f85bc7d383d871fdc0408ed6ff1445d
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2025-01-03 22:13:47 +0500

    ARRAYS AND LOOP

---
+++ day17.js
@@ -0,0 +1,36 @@
+// LOOPS IN JS
+
+// 1 - FOR LOOP
+// 2 - WHILE LOOP
+// 3 - DO WHILE LOOP
+
+
+// FOR LOOP
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+
+// TABLES IN FOR LOOP
+const tableNumber = 12;
+
+for (let i = 1; i <= 10; i++) {
+
+    console.log(`${tableNumber} * ${i} = ${tableNumber * i}`)
+}
+
+//2 * 1 = 2
+//2 * 2 = 4
+//2 * 3 = 6
+
+
+
+
+const numbers = ["Zohaib", "Hamza", "Bisma", "Tauseeq"];
+
+// for (let i = 0; i <= numbers.length; i++) {
+//     console.log(numbers[i], i)
+// }
+
+
+numbers.sort().map((rehaan) => console.log(rehaan))
